6a435c9e73d8d939711b12eaf668a752bcae6dc3
EpicWerf <62124316+EpicWerf@users.noreply.github.com>
2021-03-29 08:42:58 -0600

    Add project files.

---
+++ Components/BowlingTeamViewComponent.cs
@@ -0,0 +1,25 @@
+﻿using Assignment_10.Models;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Assignment_10.Components
+{
+    public class BowlingTeamViewComponent : ViewComponent
+    {
+        private BowlingLeagueContext context;
+        public BowlingTeamViewComponent (BowlingLeagueContext ctx)
+        {
+            context = ctx;
+        }
+
+        public IViewComponentResult Invoke()
+        {
+            return View(context.Teams
+                .Distinct()
+                .OrderBy(b => b));
+        }
+    }
+}
